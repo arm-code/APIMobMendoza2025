408aad832adb12fab33c0e66e677b950edf6b2c7
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2024-12-05 22:24:36 -0700

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require('express');
+const cors = require("cors");
 const bodyParser = require('body-parser')
 const ProductsRoutes = require('./routes/ProducstRoutes')
 const dotenv = require('dotenv')
 
 dotenv.config()
 const app = express()
+app.use(cors());
 const PORT = process.env.DB_PORT || 3000
 
 app.use(bodyParser.json())
