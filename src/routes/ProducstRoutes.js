c72f7a5997b66c4dae8bfa1115a0a084e6e70e4e
Alexis Romero Mendoza <al162917@alumnos.uacj.mx>
2024-12-09 12:39:46 -0700

    adding route aal packs

--- src/routes/ProducstRoutes.js
+++ src/routes/ProducstRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const productsController = require('../controllers/ProductsController')
 
 router.get('/', productsController.getAllProducts)
+router.get('/packs', productsController.getAllPacks)
 
 module.exports = router
