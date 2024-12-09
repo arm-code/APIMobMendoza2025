5ca6e24115ad93a42f2b4fadb77d24f2c0fad6a8
Alexis Romero Mendoza <64325158+arm-code@users.noreply.github.com>
2024-12-09 11:53:12 -0700

    Revert "adding new route all packs"

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -10,14 +10,4 @@ const getAllProducts = async(req, res) =>{
     }
 }
 
-const getAllPacks = async(req, res) => {
-    try {
-        const packs = await productsModel.getAllPacks();
-        res.json(packs)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-        
-    }
-}
-
-module.exports = {getAllProducts, getAllPacks}
+module.exports = {getAllProducts}
--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -1,40 +1,15 @@
-const pg = require('../config/db');
+const pg = require('../config/db')
 
-const getAllProducts = async () => {
-  const query = 'SELECT id_producto, nombre, descripcion, precio_renta, estado FROM productos;';
-  try {
-    const res = await pg.query(query);
-    return res.rows;
-  } catch (error) {
-    throw error;
-  }
-};
+const getAllProducts = async() =>{
 
-const getAllPacks = async () => {
-  // Consulta SQL para mostrar productos de tipo paquete en columnas separadas con los productos que integran el paquete
-
-  const query = `SELECT 
-    p.nombre AS nombre,
-    p.precio,
-    MAX(CASE WHEN pr.nombre = 'Mesa Grande' THEN pr.nombre ELSE NULL END) AS Producto1,
-    MAX(CASE WHEN pr.nombre = 'Silla Plegable' THEN pr.nombre ELSE NULL END) AS Producto2,    
-    MAX(CASE WHEN pr.nombre = 'Mesa Grande' THEN dp.cantidad ELSE NULL END) AS Cantidad1,
-    MAX(CASE WHEN pr.nombre = 'Silla Plegable' THEN dp.cantidad ELSE NULL END) AS Cantidad2
-FROM 
-    Paquetes p
-    INNER JOIN DetallePaquetes dp ON p.id_paquete = dp.id_paquete
-    INNER JOIN Productos pr ON dp.id_producto = pr.id_producto
-GROUP BY 
-    p.id_paquete, p.nombre, p.precio
-ORDER BY 
-    p.id_paquete;`;
-
+    const query ='SELECT * FROM productos;'
     try {
-        const res = await pg.query(query);
-        return res.rows;
-      } catch (error) {
-        throw error;
-      }
-};
+        const res = await pg.query(query)
+        return res.rows
+    } catch (error) {
+        throw error
+    }
 
-module.exports = { getAllProducts, getAllPacks };
+}
+
+module.exports ={ getAllProducts} 
